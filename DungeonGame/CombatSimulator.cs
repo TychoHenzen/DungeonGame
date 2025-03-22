59544d15516be584e2cef0b486a6905f54696447
Sirius)(Black <Sirius_Black9999@hotmail.com>
2025-03-22 23:57:04 +0100

    refactor: Remove SignatureHelper class from CombatSimulator

--- DungeonGame/CombatSimulator.cs
+++ DungeonGame/CombatSimulator.cs
@@ -5,35 +5,6 @@ using System.Linq;
 namespace DungeonGame;
 
 /// <summary>
-/// Helper class for signature-related operations
-/// </summary>
-public static class SignatureHelper
-{
-    /// <summary>
-    /// Calculates Euclidean distance between two signatures
-    /// </summary>
-    public static float CalculateDistance(float[] sig1, float[] sig2)
-    {
-        float sumSquaredDiffs = 0;
-
-        for (int i = 0; i < sig1.Length; i++)
-        {
-            sumSquaredDiffs += (sig1[i] - sig2[i]) * (sig1[i] - sig2[i]);
-        }
-
-        return (float)Math.Sqrt(sumSquaredDiffs);
-    }
-
-    /// <summary>
-    /// Calculates similarity between two signatures (1 = identical, 0 = completely different)
-    /// </summary>
-    public static float CalculateSimilarity(float[] sig1, float[] sig2)
-    {
-        return 1 - CalculateDistance(sig1, sig2) / 4f;
-    }
-}
-
-/// <summary>
 /// Combat simulator that handles dungeon run simulations
 /// </summary>
 public class CombatSimulator
